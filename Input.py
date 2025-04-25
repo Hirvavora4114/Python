b3f35306037bc6520441f475f1e424580503d548
Hirvavora4114 <hirvavora4114@gmail.com>
2025-04-25 19:58:52 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,11 @@
+name=input("enter your name: ")
+print("Welcome",name)
+
+age=input("enter your age:")
+print("you entered",age)
+
+val=int(input("enter some value: ")) # int("5")
+print(type(val),val)
+
+val=float(input("enter some value: "))
+print(type(val),val)
---
+++ practice1.py
@@ -0,0 +1,3 @@
+num1=int(input("enter num1: "))
+num2=int(input("enter num2: "))
+print("sum=",num1+num2)
