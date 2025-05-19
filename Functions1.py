081da0f1a71869448ce2a90e98accd1cbc0762a6
Hirvavora4114 <hirvavora4114@gmail.com>
2025-05-19 20:31:21 +0530

    Add files via upload

---
+++ Functions1.py
@@ -0,0 +1,13 @@
+#function definition
+def calc_sum(a,b): #parameters
+    return a+b
+
+sum=calc_sum(1,2) #function call; arguments
+print(sum)
+
+
+def print_hello():
+    print("hello")
+
+output=print_hello()
+print(output) #None
