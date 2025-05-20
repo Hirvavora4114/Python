7b2063b33bf73ade7418c5cef7e0db27ea9375aa
Hirvavora4114 <hirvavora4114@gmail.com>
2025-05-20 19:00:34 +0530

    Add files via upload

---
+++ Built-in Functions.py
@@ -0,0 +1,17 @@
+print("apnacollege",end=" & ") #sep=" "
+print("hirvavora") #end="\n"
+
+length=len("apnacollege")
+print(length)
+
+name="hirvavora"
+print(type(name))
+
+even=(range(2,11,2))
+
+print(even[0])
+print(even[1])
+print(even[2])
+print(even[3])
+print(even[4])
+
