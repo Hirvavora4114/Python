a1663e483b6fab7ccd221336db25688537a77748
Hirvavora4114 <hirvavora4114@gmail.com>
2025-05-23 13:59:35 +0530

    Add files via upload

---
+++ practice33.py
@@ -0,0 +1,9 @@
+#Write a function to take a number as input and check whether it is even or odd.
+def even_odd():
+    num=int(input("enter number: "))
+    if (num%2==0):
+        print("EVEN")
+    else:
+        print("ODD")
+
+even_odd()
